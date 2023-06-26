1e42a13f28a938e80c022893dcf855aa385760ab
kijihn22 <dekillji44hn@yahoo.co.jp>
2023-06-26 11:28:09 +0900

    成果物をコミット

---
+++ Sample_Games/Assets/Gimmicks/PasswordButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PasswordButton : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI numberText;
+    public int number;
+
+    private void Start()
+    {
+    }
+
+    // 実行されたら数値を変える
+    public void OnClickThis()
+    {
+    }
+}
